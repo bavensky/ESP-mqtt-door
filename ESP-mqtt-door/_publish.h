a2b5132a7d8395e4dabcde68a535aae42ee2b2de
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2016-10-24 19:20:55 +0700

    first commit

---
+++ ESP-mqtt-door/_publish.h
@@ -0,0 +1,19 @@
+#include <MqttConnector.h>
+
+extern float t_dht;
+extern float h_dht;
+extern int count;
+extern const char* DEVICE_NAME;
+
+MqttConnector::prepare_data_hook_t on_prepare_data =
+[&](JsonObject *root) -> void {
+
+    JsonObject& data = (*root)["d"];
+    JsonObject& info = (*root)["info"];
+
+    data["myName"] = DEVICE_NAME;
+    data["tags"] = "inside,cmmc";
+    data["temp"] = t_dht;
+    data["humid"] = h_dht;
+    data["count"] = count;
+};
---
+++ ESP-mqtt-door/_receive.h
@@ -0,0 +1,16 @@
+/* Channel ESP-NOW */
+extern int relay;
+
+PubSubClient::callback_t on_message_arrived = 
+[&](const MQTT::Publish & pub) -> void {
+    String topic = pub.topic();
+    String payload = pub.payload_string();
+    String text = topic + " => " + payload;
+    
+    if(payload == "ON")  {
+      digitalWrite(relay, HIGH);
+    } else if(payload == "OFF") {
+      digitalWrite(relay, LOW);
+    }
+    Serial.println(text);
+ };
